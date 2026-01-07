21e3aabba8ed9e3726bc25d4dba05cc97166e271
Yushi <yushari@seznam.cz>
2026-01-07 20:49:52 +0100

    removed unnecessary comments

--- src/scripts/nav-toggle.js
+++ src/scripts/nav-toggle.js
@@ -3,7 +3,7 @@ const closeBtn = document.querySelector('.close__btn');
 const navMenu = document.querySelector('.nav__menu');
 const dropdownBtns = document.querySelectorAll('.dropdown__btn');
 
-// Open / close mobile nav
+
 hamburger.addEventListener('click', () => {
     navMenu.classList.add('active');
     hamburger.classList.add('hidden');
@@ -15,17 +15,15 @@ closeBtn.addEventListener('click', () => {
     hamburger.classList.remove('hidden');
     closeBtn.classList.remove('active');
 
-    // close all dropdowns when menu closes
     document.querySelectorAll('.dropdown.active')
         .forEach(d => d.classList.remove('active'));
 });
 
-// Dropdown logic (ONLY on nav buttons)
+
 dropdownBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const currentDropdown = btn.nextElementSibling;
 
-        // Optional: close other dropdowns
         document.querySelectorAll('.dropdown').forEach(dropdown => {
             if (dropdown !== currentDropdown) {
                 dropdown.classList.remove('active');
