79b3a03203cad473fdc1c02ab85201a422a71494
Yushi <yushari@seznam.cz>
2026-01-15 14:34:05 +0100

    created logic for swapping aria extended to true from false

--- src/scripts/nav-toggle.js
+++ src/scripts/nav-toggle.js
@@ -3,13 +3,14 @@ const closeBtn = document.querySelector('.close__btn');
 const navMenu = document.querySelector('.nav__menu');
 const dropdownBtns = document.querySelectorAll('.dropdown__btn');
 
-
+// Hamburger Logic //
 hamburger.addEventListener('click', () => {
     navMenu.classList.add('active');
     hamburger.classList.add('hidden');
     closeBtn.classList.add('active');
 });
 
+// Close Button Logic //
 closeBtn.addEventListener('click', () => {
     navMenu.classList.remove('active');
     hamburger.classList.remove('hidden');
@@ -19,7 +20,7 @@ closeBtn.addEventListener('click', () => {
         .forEach(d => d.classList.remove('active'));
 });
 
-
+// DropDown Menu Logic //
 dropdownBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         const currentDropdown = btn.nextElementSibling;
@@ -32,4 +33,11 @@ dropdownBtns.forEach(btn => {
 
         currentDropdown.classList.toggle('active');
     });
+});
+
+document.querySelectorAll(".dropdown__btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const expanded = btn.getAttribute("aria-expanded") === "true";
+    btn.setAttribute("aria-expanded", !expanded);
+  });
 });
